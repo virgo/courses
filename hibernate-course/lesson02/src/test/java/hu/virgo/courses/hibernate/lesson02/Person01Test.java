c1086d639cf461fe4bdce98514263027e82fc892
forgo.zoltan <Almafaa_123>
2018-04-16 12:05:15 +0200

    lesson02 skeleton

--- hibernate-course/lesson02/src/test/java/hu/virgo/courses/hibernate/lesson02/Person01Test.java
+++ hibernate-course/lesson02/src/test/java/hu/virgo/courses/hibernate/lesson02/Person01Test.java
@@ -1,25 +1,32 @@
-package hu.virgo.courses.hibernate.lesson01;
+package hu.virgo.courses.hibernate.lesson02;
 
 import eu.drus.jpa.unit.api.JpaUnit;
-import hu.virgo.courses.hibernate.lesson01.model.Person;
+import hu.virgo.courses.hibernate.lesson02.model.Person;
+import hu.virgo.courses.hibernate.lesson02.model.Task;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.testng.Assert;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import java.time.LocalDateTime;
+import java.time.temporal.ChronoUnit;
+import java.util.ArrayList;
+import java.util.List;
 
 @ExtendWith(JpaUnit.class)
 public class Person01Test {
 
 	@PersistenceContext(unitName = "course_test")
 	private EntityManager em;
+	private long id;
 
 	@Test
-	public void dummy() {
+	public void adummy() {
 		Person p = new Person();
 		p.setName("Ference Hill");
 		em.persist(p);
+
 		Assert.assertNotNull(p.getId());
 	}
 }
